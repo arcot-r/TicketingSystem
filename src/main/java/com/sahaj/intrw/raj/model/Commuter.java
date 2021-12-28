dca4574aac5aece58331df0cbdff53bec273eb42
Rajanikanth Arcot <arcot.r@gmail.com>
2021-12-28 17:54:31 +0530

    model classes

--- src/main/java/com/sahaj/intrw/raj/model/Commuter.java
+++ src/main/java/com/sahaj/intrw/raj/model/Commuter.java
@@ -1,5 +1,53 @@
 package com.sahaj.intrw.raj.model;
 
+import java.util.List;
+
 public class Commuter {
+	List<Trip> tripList;
+	String name;
+	boolean hasDailyCapReached;
+	boolean hasWeeklyCapReached;
+	Integer cumulatedDailyFare = 0;
+	Integer cumulativeWeeklyFare = 0;
+
+	public List<Trip> getTripList() {
+		return tripList;
+	}
+
+	public void setTripList(List<Trip> tripList) {
+		this.tripList = tripList;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean hasDailyCapReached() {
+		return hasDailyCapReached;
+	}
+
+	public boolean hasWeeklyCapReached() {
+		return hasWeeklyCapReached;
+	}
+
+	public Integer getCumulatedDailyFare() {
+		return cumulatedDailyFare;
+	}
+
+	public void setCumulatedDailyFare(Integer cumulatedDailyFare) {
+		this.cumulatedDailyFare += cumulatedDailyFare;
+	}
+
+	public Integer getCumulativeWeeklyFare() {
+		return cumulativeWeeklyFare;
+	}
+
+	public void setCumulativeWeeklyFare(Integer cumulativeWeeklyFare) {
+		this.cumulativeWeeklyFare += cumulativeWeeklyFare;
+	}
 
 }
--- src/main/java/com/sahaj/intrw/raj/model/Trip.java
+++ src/main/java/com/sahaj/intrw/raj/model/Trip.java
@@ -1,5 +1,59 @@
 package com.sahaj.intrw.raj.model;
 
+import java.time.DayOfWeek;
+import java.util.Calendar;
+
 public class Trip {
+	private DayOfWeek day;
+	private Integer fromZone;
+	private Integer toZone;
+	private Integer calculatedFare;
+	private Calendar tripTime;
+
+	public DayOfWeek getDay() {
+		return day;
+	}
+
+	public void setDay(DayOfWeek day) {
+		this.day = day;
+	}
+
+	public Integer getFromZone() {
+		return fromZone;
+	}
+
+	public void setFromZone(Integer fromZone) {
+		this.fromZone = fromZone;
+	}
+
+	public Integer getToZone() {
+		return toZone;
+	}
+
+	public void setToZone(Integer toZone) {
+		this.toZone = toZone;
+	}
+
+	public Integer getCalculatedFare() {
+		return calculatedFare;
+	}
+
+	public void setCalculatedFare(Integer calculatedFare) {
+		this.calculatedFare = calculatedFare;
+	}
+
+	public Calendar getTripTime() {
+		return tripTime;
+	}
+
+	public void setTripTime(Calendar tripTime) {
+		this.tripTime = tripTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Trip [day=" + day + ", fromZone=" + fromZone + ", toZone=" + toZone + ", calculatedFare="
+				+ calculatedFare + ", tripTime=" + tripTime + "]";
+	}
 
 }
